bb6941ba72a59ced4f58654be22d74af1a6c3001
Netvoiangel <110711364+Netvoiangel@users.noreply.github.com>
2022-10-05 21:31:37 +0300

    Initial Commit

---
+++ structs_06.10.22/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  structs_06.10.22
+//
+//  Created by timur on 05.10.2022.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
